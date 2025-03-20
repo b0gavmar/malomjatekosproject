a8225fa563e1a4ec61cacd9133a5dc92ee17b513
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-20 13:14:27 +0100

    1. feladat

---
+++ MalomJatekosProject/Models/Jatekos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalomJatekosProject.Models
+{
+    public class Jatekos
+    {
+        public int StartingPoints { get;}
+        public int Points { get; private set; }
+        public string Name { get; set; }
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public Jatekos(int startingPoints, string name)
+        {
+            if(startingPoints < 0)
+            {
+                throw new ArgumentException("A kezdőpontok száma nem lehet negatív!");
+            }
+            StartingPoints = startingPoints;
+            Points = startingPoints;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Id}) -> {Points} pont";
+        }
+    }
+}
---
+++ MalomJatekosProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using MalomJatekosProject.Models;
+
+Console.WriteLine("1. feladat");
+try
+{
+    Jatekos jatekos = new Jatekos(-100, "Negativ Noel");
+}catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Jatekos jatekos2 = new Jatekos(100, "Pozitiv Pista");
+Console.WriteLine(jatekos2);
